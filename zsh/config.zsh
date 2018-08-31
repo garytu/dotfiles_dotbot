36781f7fbeaa6a819625e772eef68cbc5582621e
Gary Tu <garytu@tw.ibm.com>
2018-08-31 14:19:32 +0800

    Using antibody

--- zsh/config.zsh
+++ zsh/config.zsh
@@ -4,6 +4,15 @@ HISTFILE=~/.zsh_history
 HISTSIZE=10000
 SAVEHIST=$HISTSIZE
 
-setopt appendhistory
-setopt sharehistory
-setopt incappendhistory
+setopt append_history           # Dont overwrite history
+setopt extended_history         # Also record time and duration of commands.
+setopt share_history            # Share history between multiple shells
+setopt hist_expire_dups_first   # Clear duplicates when trimming internal hist.
+setopt hist_find_no_dups        # Dont display duplicates during searches.
+setopt hist_ignore_dups         # Ignore consecutive duplicates.
+setopt hist_ignore_all_dups     # Remember only one unique copy of the command.
+setopt hist_reduce_blanks       # Remove superfluous blanks.
+setopt hist_save_no_dups        # Omit older commands in favor of newer ones.
+
+[[ -d ~/bin ]] && export PATH=~/bin:$PATH
+[[ -d ~/go/bin ]] && export PATH=~/go/bin:$PATH
