7aad5fa25115ffe2b3d2f86278aec003eb1afeb3
Gary Tu <garytu@tw.ibm.com>
2018-09-04 10:44:54 +0800

    Updates

--- zsh/bindkey.zsh
+++ zsh/bindkey.zsh
@@ -1,9 +1,18 @@
 
 # Preset iTerm2's keymapping with  Natural Text Editing 
-bindkey "^[b" backward-word
-bindkey "^[f" forward-word
+if [[ $OSTYPE = (darwin)* ]]; then
+bindkey "^[[1;3D" backward-word
+bindkey "^[[1;3C" forward-word
 bindkey "^[[1~" beginning-of-line
 bindkey "^[[4~" end-of-line
+fi
+
+if [[ $OSTYPE = (linux)* ]]; then
+# bindkey "^[b" backward-word
+bindkey "∑" forward-word # option(alt)+w
+bindkey "^[[1~" beginning-of-line
+bindkey "^[[4~" end-of-line
+fi
 
 # zsh-users/zsh-history-substring-search
 bindkey '^[[A' history-substring-search-up
